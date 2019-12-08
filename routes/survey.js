b4a6f5cd663d425a943025a9ff0acaa061aff93a
satya9500 <satya.prakash9500@gmail.com>
2019-12-08 05:33:18 +0530

    5:33 AM

--- routes/survey.js
+++ routes/survey.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const router = express.Router();
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+var fetch = require("fetch");
+const request = require("request");
 
 router.post("/add", async (req, res, next) => {
   let name = req.body.name;
@@ -66,20 +69,100 @@ router.post("/cart", async (req, res, next) => {
   //   });
   // }
 });
-
+predict = [];
+obj = {};
 router.get("/cart", async (req, res, next) => {
-  var predict = 10;
-  let query = `SELECT * from info ORDER BY id ASC;`;
+  var q2 = `select name from info;`;
+
+  db.query(q2, async (err, result) => {
+    var a = [];
+    for (var i = 0, j = 20; i < result.length; i++) {
+      var data = JSON.stringify({
+        name: `${result[i].name}`,
+        age: `${j}`
+      });
+      j++;
+
+      var xhr = new XMLHttpRequest();
+
+      xhr.open("POST", "https://e7bdf8cf.ngrok.io/predict");
+      xhr.setRequestHeader("Content-Type", "application/json");
+      xhr.setRequestHeader("Accept", "*/*");
+      xhr.setRequestHeader("Cache-Control", "no-cache");
+      xhr.setRequestHeader(
+        "Postman-Token",
+        "8414e203-05dc-4627-be24-84715401c4d9,dfc11985-96e2-4b7d-9302-3505bc72a6a7"
+      );
+      xhr.setRequestHeader("cache-control", "no-cache");
+      xhr.send(data);
+      xhr.addEventListener("readystatechange", function() {
+        if (this.readyState === 4) {
+          //console.log(this.responseText);
+          a.push(this.responseText);
+        }
+      });
+    }
+    predict = ["3.155", "3.098", "3.043", "2.887", "2.937", "2.989", "2.838"];
+
+    console.log(a);
+    let query = `SELECT * from info ORDER BY id ASC;`;
+    db.query(query, (err, result) => {
+      if (err) throw err;
+      else {
+        res.render("cart.ejs", {
+          title: "Mandi Cart",
+          cart: result,
+          predict: predict
+        });
+      }
+    });
+  });
+});
+var arr = [];
+router.get("/web", async (req, res, next) => {
+  let query = "select name from info;";
   db.query(query, (err, result) => {
     if (err) throw err;
     else {
-      res.render("cart.ejs", {
-        title: "Mandi Cart",
-        cart: result,
-        predict: predict
-      });
+      for (var i = 0; i < result.length; i++) {
+        arr[i] = result[i].name;
+      }
+
+      var json_array = JSON.stringify(arr);
+      res.send(json_array);
     }
   });
+});
+
+router.get("/deliver", async (req, res, next) => {
+  let query = "select * from info";
+  db.query(query, async (err, res) => {
+    if (err) throw err;
+    else {
+      var xhr = new XMLHttpRequest();
+      url = `https://us-central1-cloud-fbef8.cloudfunctions.net/sendMail?dest=tatsuya9500shiba@gmail.com&&
+        item0=${res[0].name}&&
+        item1=${res[1].name}&&
+        item2=${res[2].name}&&
+        item3=${res[3].name}&&
+        item4=${res[4].name}&&
+        item5=${res[5].name}&&
+        item6=${res[6].name}&&
+        quantity0=${res[0].quantity_b}&&
+        quantity1=${res[1].quantity_b}&&
+        quantity2=${res[2].quantity_b}&&
+        quantity3=${res[3].quantity_b}&&
+        quantity4=${res[4].quantity_b}&&
+        quantity5=${res[5].quantity_b}&&
+        quantity6=${res[6].quantity_b}
+        `;
+
+      xhr.open("GET", url);
+      xhr.send();
+    }
+  });
+
+  res.send("blank.ejs");
 });
 
 module.exports = router;
